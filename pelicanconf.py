153cdafa2b4edd6a105096f3c0b0e23420896e4a
jordy248 <jordan.nelson248@gmail.com>
2019-02-28 06:56:05 -0500

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+### --------------------- ###
+### general site settings ###
+### --------------------- ###
+SITEURL = 'https://jordynelson.io'
+AUTHOR = u'Jordy Nelson'
+
+SITENAME                  = u'Jordy Nelson'
+SITESUBTITLE              = u''
+SHOW_SITESUBTITLE_IN_HTML = False
+
+DEFAULT_DATE = 'fs'
+DEFAULT_DATE_FORMAT = '%d %b %Y'
+TIMEZONE = 'America/New_York'
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/jordy.nelson248'),
+          ('LinkedIn', 'https://www.linkedin.com/in/jordynelson/')
+          )
+
+# Pagination
+DEFAULT_PAGINATION = 3
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+STATIC_PATHS = ['assets']
+
+EXTRA_PATH_METADATA = {
+    'assets/robots.txt': {'path': 'robots.txt'},
+    'assets/favicon.ico': {'path': 'favicon.ico'},
+    'assets/CNAME': {'path': 'CNAME'}
+}
+
+# Post and Pages path
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+# Tags and Category path
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'catgegories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+# Author
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
+
+### Plugins
+
+# PLUGIN_PATHS = [
+#   'pelican-plugins'
+# ]
+
+# PLUGINS = [
+#   'sitemap',
+#   'neighbors',
+#   'assets'
+# ]
+
+# Sitemap
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+
+
+# Analytics
+GOOGLE_ANALYTICS = "UA-78411303-1"
+
+# Comments
+DISQUS_SITENAME = "jordynelson"
+
+### -------------- ###
+### theme settings ###
+### -------------- ###
+# directory containing theme
+THEME = 'themes/martian-pelican'
+# subdir in output dir where static files are placed
+THEME_STATIC_DIR = 'theme'
+# list of theme static paths to copy to THEME_STATIC_DIC
+THEME_STATIC_PATHS = ['static']
+
+COLOR_SCHEME_CSS = 'github.css'
+CSS_OVERRIDE = ['assets/css/myblog.css']
+
+### --------------- ###
+### output settings ###
+### --------------- ###
+# specify where pelican finds content to publish
+PATH = 'content'
+# specify where pelican publishes content
+OUTPUT_PATH = 'output'
+# disable cachine
+LOAD_CONTENT_CACHE = False
+# what slug is generated from
+SLUGIFY_SOURCE = 'title'
+# port used with `pelican --listen`
+PORT = 8000
+
+
+# AUTHORS_BIO = {
+#   "arul": {
+#     "name": "Arul",
+#     "cover": "assets/images/avatar.png",
+#     "image": "assets/images/arul_author_cover.jpg",
+#     "website": "http://blog.arulraj.net",
+#     "location": "Chennai",
+#     "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
+#   }
+# }
